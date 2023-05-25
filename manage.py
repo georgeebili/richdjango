de418b39b6481b7cad755713874721d102dbc502
Ebili George <44872084+georgeebili@users.noreply.github.com>
2023-05-25 08:20:35 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "classproject.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "richdjango.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
